02da4253ddf8c3f1add99a6666880ad40c8b2d75
SwapnilYadav786 <142115705+SwapnilYadav786@users.noreply.github.com>
2023-09-09 09:52:49 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
-
+public class product{
+public int  product(int x , int y){
+  return 0;
+  }
+  public int  product(int x , int y , int z){
+    return 0;
+  }
+  public double product(Double x , Double y){
+    return 0;
+}
 public class Main {
-
+product p = new product();
+  p.product(1,1);
+  p.product(0,0,0);
+  p.product(2.2,2.4);
 }
